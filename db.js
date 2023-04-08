3f11e0bd3c3efdc68d6347110eff3eb78021a18d
Bijit Sinha <bijitsinha630@gmail.com>
2023-04-08 12:42:07 +0530

    2nd commit

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config()
+const database = process.env.DATABASEURL;
 const connect = () => {
+
   mongoose.connect(
-    "mongodb+srv://bijit63:bijitsinha630@bijit.ldc8xto.mongodb.net/ShopProject?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}
+   
+    database,{useNewUrlParser:true,useUnifiedTopology:true}
+    
   );
+
 };
-// const connect =()=>{mongoose.connect("mongodb://0.0.0.0:27017/demoprojectdatabase")}
-module.exports = connect;
+  // const connect =()=>{mongoose.connect("mongodb://0.0.0.0:27017/demoprojectdatabase")}
+  module.exports = connect;
